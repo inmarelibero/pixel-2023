e8f18c24de757a424e3e05e6788bc97ed5e19e81
Emanuele Gaspari <inmarelibero@gmail.com>
2023-10-29 15:03:26 +0100

    wip

--- init.php
+++ init.php
@@ -2,16 +2,16 @@
 session_start();
 
 require_once(__DIR__.'/src/class/App.php');
-require_once(__DIR__.'/src/class/User.php');
 require_once(__DIR__.'/src/class/AuthenticationManager.php');
 require_once(__DIR__.'/src/class/FileManager.php');
 require_once(__DIR__.'/src/class/Logger.php');
 require_once(__DIR__.'/src/class/LoginHandler.php');
+require_once(__DIR__.'/src/class/PasswordHasher.php');
 require_once(__DIR__.'/src/class/RedirectManager.php');
 require_once(__DIR__.'/src/class/RegistrationHandler.php');
 require_once(__DIR__.'/src/class/RequestHelper.php');
 require_once(__DIR__.'/src/class/TemplateHelper.php');
-require_once(__DIR__.'/src/class/PasswordHasher.php');
+require_once(__DIR__.'/src/class/User.php');
 
 $app = new App('prod');
 $authenticationManager = new AuthenticationManager($app);
